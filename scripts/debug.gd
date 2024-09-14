f2b58314326cb7d646c1427a12e227096d0bba85
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2024-09-14 14:37:40 +0200

    Added simple player script

---
+++ scripts/debug.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+
+func _on_player_moved(position: Variant, something: Variant) -> void:
+	print(position)
+	pass # Replace with function body.
---
+++ scripts/player.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var speed = 1
+
+signal moved(position, something)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("right"):
+		velocity.x = speed
+	if Input.is_action_pressed("up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("down"):
+		velocity.y = speed
+		
+	position += velocity
+	
+	if velocity != Vector2.ZERO:
+		moved.emit(position, "hello")
