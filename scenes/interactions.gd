8305acd53827b6fc0c603d8656e51c275b4b7dd7
Tyrron <damien.kech@gmail.com>
2024-09-14 17:53:42 +0200

    wip

---
+++ scenes/interactions.gd
@@ -0,0 +1,20 @@
+extends TileMapLayer
+
+@onready var player = get_parent().get_node("Player");
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(player);
+	print("Position : " , player.position);
+	var farmable_land = get_used_cells_by_id(1);
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var player_cell = local_to_map(Vector2i(player.position))
+	var cellData = get_cell_tile_data(player_cell)
+	print("cellData : " , cellData)
+	if cellData:
+		print(cellData.get_custom_data("farmable"));
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,30 @@
 extends Node2D
 
+@onready var animatedSprite = $Sprite2D
+
 @export var speed = 1
 
 signal moved(position, something)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animatedSprite.play("idle")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed
+		animatedSprite.play("move_left")
 	if Input.is_action_pressed("right"):
 		velocity.x = speed
+		animatedSprite.play("move_right")
 	if Input.is_action_pressed("up"):
 		velocity.y = -speed
 	if Input.is_action_pressed("down"):
 		velocity.y = speed
-		
 	position += velocity
 	
 	if velocity != Vector2.ZERO:
