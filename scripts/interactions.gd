982290255e5072bcf36881b3456d72e45562027e
Tyrron <damien.kech@gmail.com>
2024-09-15 20:56:36 +0200

    fix hoe

--- scripts/interactions.gd
+++ scripts/interactions.gd
@@ -1,8 +1,7 @@
 extends TileMapLayer
 
 @onready var cursor = get_parent().get_node("Cursor");
-
-var gameManager = get_parent();
+@onready var gameManager = get_parent();
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +14,7 @@ func _process(delta: float) -> void:
 	var player_cell = local_to_map(to_local(Vector2i(cursor.position)))
 	var cellData = get_cell_tile_data(player_cell)
 	if Input.is_action_just_pressed("action"):
-		if( gameManager.time == "day") :
+		if( gameManager.night == 0) :
 			if cellData.get_custom_data("hoeable") == true :
 				set_cell(player_cell,3,Vector2i(0,0));
 				return;
