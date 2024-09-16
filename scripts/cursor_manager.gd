1cc158028b04428c46c655aaacac44b5bf5e7c55
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2024-09-16 16:49:46 +0200

    Better cursor handling

--- scripts/cursor_manager.gd
+++ scripts/cursor_manager.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
 @export var map: TileMapLayer
+var last_direction = Vector2.ZERO
+var cell_position = Vector2i(0,0)
 
-signal cursor_moved(position: Vector2)
+signal cursor_moved(position: Vector2, cell_position: Vector2i)
 
 func _on_player_moved(_position: Vector2, direction: Vector2) -> void:
-	var target_position = Vector2(
-		snappedi(_position.x + (direction.x * Globals.grid_size), Globals.grid_size),
-		snappedi(_position.y + (direction.y * Globals.grid_size), Globals.grid_size)
-	)
-	position = target_position + Vector2(
-			(-1 if direction.x == -1 else 1) * Globals.grid_size / 2,
-			(-1 if direction.y == -1 else 1) * Globals.grid_size / 2
-		)
-		
-	cursor_moved.emit(position)
-	
+	if direction.x != 0:
+		last_direction.x = direction.x
+		last_direction.y = 0
+	if direction.y != 0:
+		last_direction.y = direction.y
+		last_direction.x = 0
+
+	cell_position = map.local_to_map(_position + Vector2(
+		last_direction.x * Globals.grid_size, last_direction.y * Globals.grid_size
+	))
+
+	position = map.map_to_local(cell_position)
+
+	cursor_moved.emit(position, cell_position)
--- scripts/ground.gd
+++ scripts/ground.gd
@@ -1,6 +1,10 @@
 extends TileMapLayer
 
-@onready var cursor = get_parent().get_node("Cursor");
+const HOED_GROUND_TILE_COORD = Vector2i(0,0)
+const OUTDOOR_DAY_SOURCE_ID = 3
+const CROPS_DAY_SOURCE_ID = 4
+const OUTDOOR_NIGHT_SOURCE_ID = 6
+const CROPS_NIGHT_SOURCE_ID = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,48 +13,40 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var player_cell = local_to_map(to_local(Vector2i(cursor.position)))
-	var cellData = get_cell_tile_data(player_cell)
+	var targeted_cell = %Cursor.cell_position
+	var cellData = get_cell_tile_data(targeted_cell)
 	if Input.is_action_just_pressed("action") and GameManager.night == 0:
 		if cellData.get_custom_data("hoeable") == true :
-			set_cell(player_cell,3,Vector2i(0,0));
+			set_cell(targeted_cell, OUTDOOR_DAY_SOURCE_ID, HOED_GROUND_TILE_COORD);
 			return;
 		if cellData.get_custom_data("hoeable") == false && cellData.get_custom_data("farmable") == true :
-			set_cell(player_cell,4,Vector2i(0,0));
+			set_cell(targeted_cell, 4, HOED_GROUND_TILE_COORD);
 			return;
-	pass
-
-func is_tile_hoeable(tile_position: Vector2) -> bool:
-	var player_cell = local_to_map(to_local(tile_position))
-	var cellData = get_cell_tile_data(player_cell)
-	if cellData.get_custom_data("hoeable") == true :
-		return true
-	return false
 
 func _on_night_begin() -> void:
 	# Replace ground texture
-	var all_tile_ground = get_used_cells_by_id(3);
+	var all_tile_ground = get_used_cells_by_id(OUTDOOR_DAY_SOURCE_ID);
 	for i in all_tile_ground:
 		var atlas_cord = get_cell_atlas_coords(i);
 		var atlas_transform = get_cell_alternative_tile(i);
-		set_cell(i, 6,atlas_cord,atlas_transform);
+		set_cell(i, OUTDOOR_NIGHT_SOURCE_ID, atlas_cord,atlas_transform);
 	# Replace crops
-	var all_tile_crops = get_used_cells_by_id(4);
+	var all_tile_crops = get_used_cells_by_id(CROPS_DAY_SOURCE_ID);
 	for i in all_tile_crops:
 		var atlas_cord = get_cell_atlas_coords(i);
 		var atlas_transform = get_cell_alternative_tile(i);
-		set_cell(i, 5,atlas_cord,atlas_transform);
+		set_cell(i, CROPS_NIGHT_SOURCE_ID, atlas_cord,atlas_transform);
 
 func _on_night_end() -> void:
 	# Replace ground texture
-	var all_tile_ground = get_used_cells_by_id(6);
+	var all_tile_ground = get_used_cells_by_id(OUTDOOR_NIGHT_SOURCE_ID);
 	for i in all_tile_ground:
 		var atlas_cord = get_cell_atlas_coords(i);
 		var atlas_transform = get_cell_alternative_tile(i);
-		set_cell(i, 3, atlas_cord,atlas_transform);
+		set_cell(i, OUTDOOR_DAY_SOURCE_ID, atlas_cord,atlas_transform);
 	# Replace crops
-	var all_tile_crops = get_used_cells_by_id(5);
+	var all_tile_crops = get_used_cells_by_id(CROPS_NIGHT_SOURCE_ID);
 	for i in all_tile_crops:
 		var atlas_cord = get_cell_atlas_coords(i);
 		var atlas_transform = get_cell_alternative_tile(i);
-		set_cell(i, 4, atlas_cord,atlas_transform);
+		set_cell(i, CROPS_DAY_SOURCE_ID, atlas_cord,atlas_transform);
