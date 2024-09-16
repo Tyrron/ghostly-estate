bef33eeb5a7657b451aecb1a5bfae33aa0e568e7
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2024-09-16 09:54:52 +0200

    Updated resolution handling to match GB original

--- globals.gd
+++ globals.gd
@@ -1,3 +1,9 @@
 extends Node
 
 var grid_size = 16
+var window_scale = 5
+
+func _ready() -> void:
+	var window = get_window()
+	window.size *= window_scale
+	
--- scripts/main_camera.gd
+++ scripts/main_camera.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+func _ready() -> void:
+	self.position = %Player.position
 
 func _on_player_moved(player_position: Vector2, direction: Vector2) -> void:
 	self.position = player_position
